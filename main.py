1b824a4f8b9bfdde4f51b520374e677b06155000
The Cheaterman <the.cheaterman@gmail.com>
2017-09-01 20:16:06 +0200

    PEP8 & general fixes

--- main.py
+++ main.py
@@ -2,26 +2,28 @@ import os
 import time
 from random import randint
 
-while 1 :
+
+while True:
     h = int(input("heure : "))
     m = int(input("minute : "))
     s = int(input("seconde : "))
-    while 1:
+
+    while True:
         heure = time.localtime()
         music = randint(0, 6)
         if (heure.tm_hour == h and heure.tm_min == m and heure.tm_sec == s):
-            if (music == 0) :
+            if music == 0:
                 os.startfile("Enter_Sandman.flac")
-            elif (music == 1) :
+            elif music == 1:
                 os.startfile("A_demon_s_fate.flac")
-            else :
+            else:
                 os.startfile("Blind.flac")
-			# ...
+            # ...
 
             time.sleep(2)
 
             os.system("pause")
             os.system("taskkill /f /im foobar2000.exe")
             break
-        else :
+        else:
             time.sleep(1)
