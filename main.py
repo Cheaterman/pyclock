b3f9febb88a4a900e5875586602c627c6ee9c182
The Cheaterman <the.cheaterman@gmail.com>
2017-09-01 20:45:36 +0200

    Find music files for alarm clock in a configurable folder
    "*.flac" files and "music" folder by default

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # encoding: utf8
 
 import datetime
+import glob
 import os
 import random
 import time
 
 DATE_FORMAT = '%H:%M:%S'
+MUSIC_FOLDER = 'music'
+MUSIC_EXTENSION = 'flac'
 
 try:
     input = raw_input
@@ -16,20 +19,24 @@ except NameError:
 while True:
     raw_data = input(u'Heure du réveil (HH:MM:SS) : '.encode('utf8'))
     target = datetime.time(*[int(item) for item in raw_data.split(':')])
+    music_files = glob.glob(os.path.join(MUSIC_FOLDER, '*.' + MUSIC_EXTENSION))
+
+    if not music_files:
+        print(
+            u'Erreur: Aucun fichier présent dans le dossier {}.'.format(
+                MUSIC_FOLDER
+            )
+        )
+        continue
 
     while True:
-        music = random.randint(0, 6)
         if(
             datetime.datetime.now()
             .strftime(DATE_FORMAT) == target.strftime(DATE_FORMAT)
         ):
-            if music == 0:
-                os.startfile("Enter_Sandman.flac")
-            elif music == 1:
-                os.startfile("A_demon_s_fate.flac")
-            else:
-                os.startfile("Blind.flac")
-            # ...
+            filename = random.choice(music_files)
+            print('Playing: {}'.format(filename))
+            os.startfile(filename)
 
             time.sleep(2)
 
