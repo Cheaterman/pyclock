c07509a6f0da4cafa59e396751f6ddb5c7374da9
The Cheaterman <the.cheaterman@gmail.com>
2017-09-01 22:19:12 +0200

    Add basic value checking, current time notification

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # encoding: utf8
 
+from threading import Thread
 import datetime
 import glob
 import os
@@ -16,10 +17,31 @@ try:
 except NameError:
     pass
 
+should_tell_time = False
+
+def tell_time():
+    while True:
+        if should_tell_time:
+            print(
+                'Il est actuellement: {}'.format(
+                    datetime.datetime.now().strftime(DATE_FORMAT)
+                )
+            )
+        time.sleep(1)
+
+thread = Thread(target=tell_time)
+thread.daemon = True
+thread.start()
+
 
 while True:
-    raw_data = input('Heure du réveil (HH:MM:SS) : ')
-    target = datetime.time(*[int(item) for item in raw_data.split(':')])
+    target = None
+    while not target:
+        raw_data = input('Heure du réveil (HH:MM:SS) : ')
+        try:
+            target = datetime.time(*[int(item) for item in raw_data.split(':')])
+        except ValueError:
+            pass
     music_files = glob.glob(os.path.join(MUSIC_FOLDER, '*.' + MUSIC_EXTENSION))
 
     if not music_files:
@@ -29,6 +51,8 @@ while True:
             )
         )
         continue
+
+    should_tell_time = True
 
     while True:
         if(
@@ -41,3 +65,5 @@ while True:
             break
         else:
             time.sleep(.5)
+
+    should_tell_time = False
